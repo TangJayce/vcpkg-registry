00a425040583f63ab164cbcb2ee4b29928f5f56d
jkchen <jkchen@gudsen.cn>
2024-01-26 19:22:40 +0800

    [moza-module-b] 更新到 0.1.0#2 [moza-module-c] 更新到 0.1.0#2

--- ports/moza-module-b/portfile.cmake
+++ ports/moza-module-b/portfile.cmake
@@ -1,14 +1,15 @@
 ﻿vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL D:/Projects/vcpkg-playground/example-remote/moza-module-b/.git
-    REF 59affb821c040f2161bc131d613595b2dfa4399b
+    REF 51513a7103d25b6125c3ab385036ed9dda6a5d93
 )
 
+# 需要在 triple 中定义 ${CMAKE_PREFIX_PATH} 变量
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS
+    OPTIONS
         ${FEATURE_OPTIONS}
-        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} # 来自 custom-overlay-triplets 中 triple 定义的变量
+        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
         -DENABLE_DOCS=OFF
         -DENABLE_TESTS=OFF
 )
@@ -18,7 +19,7 @@ vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
- 
+
 # Remove debug include
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
--- ports/moza-module-c/portfile.cmake
+++ ports/moza-module-c/portfile.cmake
@@ -1,14 +1,15 @@
 ﻿vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL D:/Projects/vcpkg-playground/example-remote/moza-module-c/.git
-    REF e56ad3c8e7a87e48647c4ebacd709052cfaa99ef
+    REF 7fc92fd59e446bb438a9ca405afd25d2177b3ada
 )
 
+# 需要在 triple 中定义 ${CMAKE_PREFIX_PATH} 变量
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS
+    OPTIONS
         ${FEATURE_OPTIONS}
-        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} # 来自 custom-overlay-triplets 中 triple 定义的变量
+        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
         -DENABLE_DOCS=OFF
         -DENABLE_TESTS=OFF
 )
@@ -18,7 +19,7 @@ vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
- 
+
 # Remove debug include
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
